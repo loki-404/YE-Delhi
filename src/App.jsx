b334fbe537c3e1e8a3a0bb36ed53aebaff68b349
Dev <runasroot3@gmail.com>
2026-02-20 13:28:14 +0530

    reverted

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import DondaDelhi from "./donda-delhi";
-import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-  return (
-    <>
-      <DondaDelhi />
-      <Analytics />
-    </>
-  );
+  return <DondaDelhi />;
 }
 
 export default App;
